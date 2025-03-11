fde053b58bfd8021dc16b9c4cc5bad83569aff36
afullhart <58483164+afullhart@users.noreply.github.com>
2025-03-11 15:14:56 -0700

    Update Data_to_Pars_Daily.py

--- Data_to_Pars_Daily.py
+++ Data_to_Pars_Daily.py
@@ -16,7 +16,6 @@ import os
 dataFolder = r'...\dataFolder'
 parFolder = r'...\dataParFolder'
 
-
 acc_threshold = 0.0001 #Made very small. Any daily accumulation less than this number is omitted.
 
 dataFiles = os.listdir(dataFolder)
@@ -27,8 +26,8 @@ def daily_pars(dataFile, dataDir, parDir):
   rows = []
   with open(os.path.join(dataDir, dataFile)) as f:
     next(f)
-    for line in f:            
-      row = line.strip('\n').split(',')
+    for line in f:
+      row = line.strip('\n').split(',')[1:]
       date = dt.datetime(year=int(row[0]), month=int(row[1]), day=int(row[2]))
       rows.append([date, float(row[3]), float(row[4]), float(row[5]), float(row[6]), float(row[7])])
 
@@ -37,20 +36,20 @@ def daily_pars(dataFile, dataDir, parDir):
   dataset['month'] = [date[0].month for date in rows]
   dataset['day'] = [date[0].day for date in rows]
   dataset['pcp in.'] = [value[1] for value in rows]
-  dataset['Tmax F'] = [value[2] for value in rows] 
-  dataset['Tmin F'] = [value[3] for value in rows] 
-  dataset['Tdew F'] = [value[4] for value in rows] 
-  dataset['Srad L'] = [value[5] for value in rows] 
-  
+  dataset['Tmax F'] = [value[2] for value in rows]
+  dataset['Tmin F'] = [value[3] for value in rows]
+  dataset['Tdew F'] = [value[4] for value in rows]
+  dataset['Srad L'] = [value[5] for value in rows]
+
   output_rows = []
   output_rows_tmp = []
   meanP_list, stddevP_list, skewP_list = [], [], []
   wd_list, ww_list = [], []
   tmax_list, tmin_list, tdew_list, tmax_sd_list, tmin_sd_list, tdew_sd_list = [], [], [], [], [], []
   srad_list, srad_sd_list = [], []
-  
+
   for mo in range(1, 13):
-      
+
     yr_mo_query = dataset.loc[(dataset['month'] == mo)]
     monthly_precip_by_year = yr_mo_query.groupby('year')['pcp in.'].apply(list)
     nonzeros = [item for month in monthly_precip_by_year for item in month if item >= acc_threshold]
@@ -64,8 +63,8 @@ def daily_pars(dataFile, dataDir, parDir):
 
       if stddevP != 0.0:
         skewP = sum([((x - meanP)/stddevP)**3 for x in nonzeros]) / (len(nonzeros) - 1)
-        skewP_list.append(skewP)               
-      else:    
+        skewP_list.append(skewP)
+      else:
         skewP_list.append(0)
 
     else:
@@ -86,7 +85,7 @@ def daily_pars(dataFile, dataDir, parDir):
     tmax_list.append(np.mean(tmax_values))
     tmin_list.append(np.mean(tmin_values))
     tdew_list.append(np.mean(tdew_values))
-    srad_list.append(np.mean(srad_values))   
+    srad_list.append(np.mean(srad_values))
     tmax_sd_list.append(np.std(tmax_values, ddof=1))
     tmin_sd_list.append(np.std(tmin_values, ddof=1))
     tdew_sd_list.append(np.std(tdew_values, ddof=1))
@@ -98,7 +97,7 @@ def daily_pars(dataFile, dataDir, parDir):
     for mo_data in monthly_precip_by_year:
 
       for i, elem in enumerate(mo_data):
-        
+
         if i < len(mo_data) - 1:
 
           if (elem) < acc_threshold and (mo_data[i+1]) >= acc_threshold:
@@ -124,12 +123,12 @@ def daily_pars(dataFile, dataDir, parDir):
         wd = wd_ct / (wd_ct + dd_ct)
       else:
         wd = 0
-  
+
       if dw_ct + ww_ct > 0:
         ww = ww_ct / (dw_ct + ww_ct)
       else:
         ww = 0
-  
+
       wd_list.append(wd)
       ww_list.append(ww)
 
@@ -147,25 +146,25 @@ def daily_pars(dataFile, dataDir, parDir):
     srad = '{:.0f}'.format(srad_list[i]) + '.'
     sradsd = '{:.1f}'.format(srad_sd_list[i]).lstrip('0')
     tdew = '{:.2f}'.format(tdew_list[i]).lstrip('0')
-    
+
     ###
     #Cases that could result in divide by zero error
     ###
-    
+
     #In case wd rounds to zero when there is non-zero monthly accumulation
     if wd == '.00' and float(meanP) >= 0.01:
-      wd = '.01' 
+      wd = '.01'
     else:
       wd = wd
-    
+
     #In case there is zero monthly accumulation
     if meanP == '.00':
       wd = '0.01'
       ww = '0.00'
     else:
         pass
-        
-    #In case it rains every day 
+
+    #In case it rains every day
     if ww == '1.00':
       ww = '.99'
     else:
@@ -174,29 +173,54 @@ def daily_pars(dataFile, dataDir, parDir):
     output_rows_tmp.append([meanP, sdevP, skewP, ww, wd, tmax, tmin, tmaxsd, tminsd, srad, sradsd, tdew])
 
   output_rows.extend(output_rows_tmp)
-  
+
   flipped = np.array(output_rows).transpose()
-  
+
   indices = ['MEAN P', 'S DEV P', 'SKEW  P', 'P(W/W)', 'P(W/D)', 'TMAX AV', 'TMIN AV', 'SD TMAX', 'SD TMIN', 'SOL.RAD', 'SD SOL', 'DEW PT']
   df = pd.DataFrame(flipped, index=indices, columns=list(range(1,13)))
   df.columns.name = dataFile
   df.to_csv(os.path.join(parDir, dataFile), index_label=dataFile)
   
+  meanP = df.loc[indices[0],:]
+  sdevP = df.loc[indices[1],:]
+  skewP = df.loc[indices[2],:]
+  ww = df.loc[indices[3],:]
+  wd = df.loc[indices[4],:]
+  tmax = df.loc[indices[5],:]
+  tmin = df.loc[indices[6],:]
+  tmaxsd = df.loc[indices[7],:]
+  tminsd = df.loc[indices[8],:]
+  srad = df.loc[indices[9],:]
+  sradsd = df.loc[indices[10],:]
+  tdew =  df.loc[indices[11],:]
+  
   with open(os.path.join(parDir, dataFile[:-4] + '.txt'), 'w') as f_out:
+    
+    gen = formatting_obj.spacing_gen_new(meanP)
+    f_out.write(' MEAN P ' + ''.join([next(gen) + str(x) + next(gen) if i < 1 else str(x) if i == 11 else str(x) + next(gen) for i, x in enumerate(meanP)]) + '\n')
+    gen = formatting_obj.spacing_gen_new(sdevP)
+    f_out.write(' S DEV P' + ''.join([next(gen) + str(x) + next(gen) if i < 1 else str(x) if i == 11 else str(x) + next(gen) for i, x in enumerate(sdevP)]) + '\n')
+    gen = formatting_obj.spacing_gen_new(skewP)
+    f_out.write(' SKEW  P' + ''.join([next(gen) + str(x) + next(gen) if i < 1 else str(x) if i == 11 else str(x) + next(gen) for i, x in enumerate(skewP)]) + '\n')
+    gen = formatting_obj.spacing_gen_new(ww)
+    f_out.write(' P(W/W) ' + ''.join([next(gen) + str(x) + next(gen) if i < 1 else str(x) if i == 11 else str(x) + next(gen) for i, x in enumerate(ww)]) + '\n')
+    gen = formatting_obj.spacing_gen_new(wd)
+    f_out.write(' P(W/D) ' + ''.join([next(gen) + str(x) + next(gen) if i < 1 else str(x) if i == 11 else str(x) + next(gen) for i, x in enumerate(wd)]) + '\n')
+    gen = formatting_obj.spacing_gen_new(tmax)
+    f_out.write(' TMAX AV' + ''.join([next(gen) + str(x) + next(gen) if i < 1 else str(x) if i == 11 else str(x) + next(gen) for i, x in enumerate(tmax)]) + '\n')
+    gen = formatting_obj.spacing_gen_new(tmin)
+    f_out.write(' TMIN AV' + ''.join([next(gen) + str(x) + next(gen) if i < 1 else str(x) if i == 11 else str(x) + next(gen) for i, x in enumerate(tmin)]) + '\n')
+    gen = formatting_obj.spacing_gen_new(tmaxsd)
+    f_out.write(' SD TMAX' + ''.join([next(gen) + str(x) + next(gen) if i < 1 else str(x) if i == 11 else str(x) + next(gen) for i, x in enumerate(tmaxsd)]) + '\n')
+    gen = formatting_obj.spacing_gen_new(tminsd)
+    f_out.write(' SD TMIN' + ''.join([next(gen) + str(x) + next(gen) if i < 1 else str(x) if i == 11 else str(x) + next(gen) for i, x in enumerate(tminsd)]) + '\n')
+    gen = formatting_obj.spacing_gen_new(srad)
+    f_out.write(' SOL.RAD' + ''.join([next(gen) + str(x) + next(gen) if i < 1 else str(x) if i == 11 else str(x) + next(gen) for i, x in enumerate(srad)]) + '\n')
+    gen = formatting_obj.spacing_gen_new(sradsd)
+    f_out.write(' SD SOL ' + ''.join([next(gen) + str(x) + next(gen) if i < 1 else str(x) if i == 11 else str(x) + next(gen) for i, x in enumerate(sradsd)]) + '\n')
+    gen = formatting_obj.spacing_gen_new(tdew)
+    f_out.write(' DEW PT ' + ''.join([next(gen) + str(x) + next(gen) if i < 1 else str(x) if i == 11 else str(x) + next(gen) for i, x in enumerate(tdew)]) + '\n')
 
-    for index in indices:
-
-      row = df.loc[index,:]
-      f_out.write(index.ljust(4) + '\t')
-      for j, elem in enumerate(row):
-
-        if j < len(row)-1:
-          f_out.write(elem.rjust(6) + '\t')
-        elif j == len(row) - 1:
-          f_out.write(elem.rjust(6) + '\n')
-        else:
-            pass
-        
   return 0
 
 
@@ -205,5 +229,3 @@ for f in dataFiles:
   output = daily_pars(f, dataFolder, parFolder)
   run_ct += 1
   print(str(run_ct) + ' / ' + str(len(dataFiles)))
-
-
